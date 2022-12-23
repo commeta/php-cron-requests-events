d949b101dc3834a2ae5f0f27b76ace47abf1ffae
Евгений <40506054+commeta@users.noreply.github.com>
2022-12-23 23:32:15 +0300

    Update trigger.php

--- trigger.php
+++ trigger.php
@@ -1,14 +1,14 @@
 <?php
 
-if((isset($_REQUEST["cron"]))) { // CRON
-	if(is_file(__DIR__.'/cron.php')) {
-		if(!file_exists(__DIR__.'/cron.dat')) touch(__DIR__.'/cron.dat');
-		if(filemtime(__DIR__.'/cron.dat') + 60 > time()) die();
-		include(__DIR__.'/cron.php');
-	}
+if(
+	isset($_REQUEST["cron"]),
+	is_file(__DIR__.'/cron.php'),
+){
+	if(!file_exists(__DIR__.'/cron.dat')) touch(__DIR__.'/cron.dat');
+	if(filemtime(__DIR__.'/cron.dat') + 60 > time()) die();
+	include(__DIR__.'/cron.php');
 	die();
 }
-
 
 if(file_exists(__DIR__.'/cron.dat')){
 	if(filemtime(__DIR__.'/cron.dat') + 60 < time()){
