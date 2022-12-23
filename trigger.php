3240c4d904b3586539f5ae22bab39ae3022db5bd
Евгений <40506054+commeta@users.noreply.github.com>
2022-12-23 22:57:06 +0300

    Create trigger.php

---
+++ trigger.php
@@ -0,0 +1,32 @@
+<?php
+
+if((isset($_REQUEST["cron"]))) { // CRON
+	if(is_file(__DIR__.'/cron.php')) {
+		if(!file_exists(__DIR__.'/cron.dat')) touch(__DIR__.'/cron.dat');
+		if(filemtime(__DIR__.'/cron.dat') + 60 > time()) die();
+		include(__DIR__.'/cron.php');
+	}
+	die();
+}
+
+
+if(file_exists(__DIR__.'/cron.dat')){
+	if(filemtime(__DIR__.'/cron.dat') + 60 < time()){
+		@fclose(
+			@fopen(
+				'https://' . strtolower(@$_SERVER["HTTP_HOST"]) . "/?cron=", 
+				'r', 
+				false, 
+				stream_context_create([
+					'http'=>[
+						'timeout' => 0.04
+					]
+				])
+			)
+		);
+	} 
+} else {
+	touch(__DIR__.'/cron.dat');
+}
+
+?>
