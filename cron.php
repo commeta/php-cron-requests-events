9d0bc7dd3d69bd9ae3085c6647b50a66e17efb4b
Евгений <40506054+commeta@users.noreply.github.com>
2022-12-23 23:22:42 +0300

    Create cron.php

---
+++ cron.php
@@ -0,0 +1,128 @@
+<?php
+
+$fp= fopen(__DIR__.'/cron.dat', "r+");
+if(flock($fp, LOCK_EX)) {
+	// check if fastcgi_finish_request is callable
+	if(is_callable('fastcgi_finish_request')) {
+		session_write_close();
+		fastcgi_finish_request();
+	}
+
+	ignore_user_abort(true);
+
+	ob_start();
+	
+	header(filter_input(INPUT_SERVER, 'SERVER_PROTOCOL', FILTER_SANITIZE_STRING).' 200 OK');
+	header('Content-Encoding: none');
+	header('Content-Length: '.ob_get_length());
+	header('Connection: close');
+
+	ob_end_flush();
+	ob_flush();
+	flush();
+
+	if (is_callable('proc_nice')) {
+		proc_nice(15);
+	}
+
+	set_time_limit(600);
+	ini_set('MAX_EXECUTION_TIME', 600);
+
+
+	//###########################################
+	// Session init
+	
+	$cs=unserialize(fread($fp, filesize(__DIR__.'/cron.dat')));
+	if(is_array($cs) ){
+		$GLOBALS['cron_session']= $cs;
+		if((int)$GLOBALS['cron_session']['finish'] + 60 > time()){
+			flock($fp, LOCK_UN);
+			die();
+		}
+	} else {
+		$GLOBALS['cron_session']= [
+			'finish'=> time()
+		];
+	}
+		
+	$GLOBALS['cron_session']['events']= [];
+	write_cron_session($fp);
+
+
+
+	//###########################################
+	// CRON Jobs
+
+	cron_session_add_event($fp, [
+		'date'=> date('m/d/Y H:i:s', time()),
+		'message'=> 'INFO: start cron',
+	]);
+
+	flock($fp, LOCK_UN);
+}
+
+fclose($fp);
+
+
+
+if(@filesize(__DIR__ . "/cron.log") >  10 * 1024 * 1024 / 5) {
+	rename(__DIR__ . "/cron.log", __DIR__ . "/cron.log" . "." . time());
+	@file_put_contents(
+    __DIR__ . "/cron.log", 
+    date('m/d/Y H:i:s',time()) . "INFO: log rotate\n", 
+    FILE_APPEND | LOCK_EX
+  );
+	
+	$the_oldest = time();
+	$log_old_file = '';
+	$log_files_size = 0;
+            
+	foreach(glob(__DIR__ . "/cron.log" . '*') as $file_log_rotate){
+		$log_files_size+= filesize($file_log_rotate);
+		if ($file_log_rotate == $this->log_file) {
+			continue;
+		}
+		
+		$log_mtime = filectime($file_log_rotate);
+		if ($log_mtime < $the_oldest) {
+			$log_old_file = $file_log_rotate;
+			$the_oldest = $log_mtime;
+		}
+	}
+
+	if ($log_files_size > 10 * 1024 * 1024) {
+		if (file_exists($log_old_file)) {
+			unlink($log_old_file);
+			@file_put_contents(
+        __DIR__ . "/cron.log", 
+        date('m/d/Y H:i:s', time()) . "INFO: log removal\n",
+        FILE_APPEND | LOCK_EX);
+		}
+	}
+}
+
+
+
+function cron_session_add_event(& $fp, $event){
+	$GLOBALS['cron_session']['finish']= time();
+	$GLOBALS['cron_session']['events'][]= $event;
+	write_cron_session($fp);
+
+	file_put_contents(
+		__DIR__ . "/cron.log",
+		implode(' ', $event) . "\n",
+		FILE_APPEND | LOCK_EX
+	);
+}
+
+
+
+function write_cron_session(& $fp){
+	$serialized= serialize($GLOBALS['cron_session']);
+
+	rewind($fp);
+	fwrite($fp, $serialized);
+	ftruncate($fp, mb_strlen($serialized));
+	fflush($fp);
+}
+?>
