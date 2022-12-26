66376355595fd39c26e4389f2589305d39f3de2c
Евгений <40506054+commeta@users.noreply.github.com>
2022-12-26 17:56:51 +0300

    Update callback_cron.php

--- callback_cron.php
+++ callback_cron.php
@@ -1,10 +1,12 @@
 <?php
 // Example callback script, for /cron/inc/callback_cron.php
 
+// Write log
 @file_put_contents(
 	CRON_LOG_FILE, 
 	microtime() . " DEBUG: start  microtime:" . 
 		print_r([
+			$GLOBALS['cron_session']['my_var'],
 			$_SERVER['QUERY_STRING'], 
 			$_SERVER['SERVER_NAME'], 
 			$_SERVER['REQUEST_METHOD'], 
@@ -12,6 +14,10 @@
 		] , true) . " \n",
 	FILE_APPEND | LOCK_EX
 );
+
+// Save sesson variables, in Job name context, autoload in next start session
+$GLOBALS['cron_session']['my_var']= 'example parameter';
+write_cron_session($fp); 
 
 
 ?>
