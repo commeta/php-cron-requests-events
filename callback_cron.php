7cd237c01d7428bf6cebe86b65cfb1e0686da01e
Евгений <40506054+commeta@users.noreply.github.com>
2022-12-28 00:40:43 +0300

    Example callback script

--- callback_cron.php
+++ callback_cron.php
@@ -1,23 +1,29 @@
 <?php
-// Example callback script, for /cron/inc/callback_cron.php
-
 // Write log
-@file_put_contents(
-	CRON_LOG_FILE, 
-	microtime() . " DEBUG: start  microtime:" . 
-		print_r([
-			$GLOBALS['cron_session']['start_counter'],
-			$_SERVER['QUERY_STRING'], 
-			$_SERVER['SERVER_NAME'], 
-			$_SERVER['REQUEST_METHOD'], 
-			$_SERVER['REQUEST_URI']
-		] , true) . " \n",
-	FILE_APPEND | LOCK_EX
-);
 
-// Auto saved sesson variables, in Job name context (multithreading), autoload in next start session
+/*
+if(CRON_LOG_FILE){
+	@file_put_contents(
+		CRON_LOG_FILE, 
+		microtime() . " DEBUG: start  microtime:" . 
+			print_r([
+				$GLOBALS['cron_session']['start_counter'],
+				$_SERVER['QUERY_STRING'], 
+				$_SERVER['SERVER_NAME'], 
+				$_SERVER['REQUEST_METHOD'], 
+				$_SERVER['REQUEST_URI']
+			] , true) . " \n",
+		FILE_APPEND | LOCK_EX
+	);
+}
+*/
+
+// Save sesson variables, in Job name context (multithreading), autoload in next start session
 if(isset($GLOBALS['cron_session']['start_counter'])){
 	$GLOBALS['cron_session']['start_counter']++;
 } else {
 	$GLOBALS['cron_session']['start_counter']= 0;
+}
+
+
 ?>
