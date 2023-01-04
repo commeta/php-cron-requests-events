6a7645423e18ecf4b885b7b8b2fbc9fe34e9aa21
Евгений <40506054+commeta@users.noreply.github.com>
2023-01-04 05:57:42 +0300

    Update callback_addressed_queue_example.php

--- callback_addressed_queue_example.php
+++ callback_addressed_queue_example.php
@@ -4,13 +4,14 @@ function queue_address_manager_extend($mode){ // example: multicore queue
 	$dat_file= dirname(CRON_DAT_FILE) . DIRECTORY_SEPARATOR . 'queue.dat';
 	$index_file= dirname(CRON_DAT_FILE) . DIRECTORY_SEPARATOR . 'queue_index.dat';
 	$frame_size= 95;
+		
 	if(!file_exists($dat_file)) touch($dat_file);
-	
+		
 	if($mode){
 		// example: multicore queue worker
 		// use:
 		// queue_address_push($multicore_long_time_micro_job); // add micro job in queue from worker process
-		
+			
 		$index= []; // index - address array, frame_cursor is key of array
 		unlink($dat_file); // reset DB file
 		touch($dat_file);
@@ -25,7 +26,7 @@ function queue_address_manager_extend($mode){ // example: multicore queue
 				'url'=> "https://multicore_long_time_micro_job?param=" . $i,
 				'count'=> $i
 			], $frame_size);
-			
+				
 			if($frame_cursor !== false) $index[$i]= $frame_cursor; 
 		}
 						
@@ -48,38 +49,72 @@ function queue_address_manager_extend($mode){ // example: multicore queue
 		// example 1, get first element
 		$multicore_long_time_micro_job= queue_address_pop($frame_size, $index[0]);
 			
-		// example 2, linear read
+		// example 2, get last - 10 element, and get first frame in callback function
+		$multicore_long_time_micro_job= queue_address_pop(
+			$frame_size, 
+			$index[count($index) - 10], 
+			false, 
+			function(& $queue_resource, & $args){
+				fseek($queue_resource, 0); // get data frame
+				$raw_frame= fread($queue_resource, $args[0]);
+				$value= unserialize(trim($raw_frame));
+					
+					
+				if(CRON_LOG_LEVEL > 3){
+					if(CRON_LOG_FILE){
+						@file_put_contents(
+							CRON_LOG_FILE, 
+								print_r([$args, $value, $raw_frame], true),
+							FILE_APPEND | LOCK_EX
+						);
+					}
+				}					
+			}
+		);
+			
+		// example 3, linear read
 		for($i= 100; $i < 800; $i++){ // execution time:  0.037011861801147, 1000 cycles, address mode
 			$multicore_long_time_micro_job= queue_address_pop($frame_size, $index[$i]);
 		}
 			
-		// example 3, replace frames in file
+		// example 4, replace frames in file
 		for($i= 10; $i < 500; $i++){ // execution time:  0.076093912124634, 1000 cycles, address mode, frame_replace
 			$multicore_long_time_micro_job= queue_address_pop($frame_size, $index[$i], true);
 			unset($index[$i]);
 		}
-			
-			
-		// example 4, random access
+		
+		
+		// example 5, random access
 		shuffle($index);
 		for($i= 0; $i < 10; $i++){// execution time: 0.035359859466553, 1000 cycles, address mode, random access
 			$multicore_long_time_micro_job= queue_address_pop($frame_size, $index[$i]);
 		}
 
-
-		// example 5, use LIFO mode
+		// example 6, use LIFO mode
 		// execution time: 0.051764011383057 end - start, 1000 cycles
 		while(true){ // example: loop from the end
 			$multicore_long_time_micro_job= queue_address_pop($frame_size);
-				
+			
 			if($multicore_long_time_micro_job === false) {
 				break 1;
 			} elseif($multicore_long_time_micro_job !== true) {
 				// $content= file_get_contents($multicore_long_time_micro_job['url']);
 				// file_put_contents('cron/temp/url-' . $multicore_long_time_micro_job['count'] . '.html', $content);
-					
+				
+				if(CRON_LOG_LEVEL > 3){
+					if(CRON_LOG_FILE){
+						@file_put_contents(
+							CRON_LOG_FILE, 
+								microtime(true) . 
+								" INFO: queue_manager " . 
+								$multicore_long_time_micro_job['count'] . " \n",
+							FILE_APPEND | LOCK_EX
+						);
+					}
+				}					
 			}
 		}
+		
 		unlink($dat_file); // reset DB file
 	}
 }	
