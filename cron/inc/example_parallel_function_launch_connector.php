9c22a65f01dbd057cf80020e130d0a229019ba44
Евгений <40506054+commeta@users.noreply.github.com>
2023-01-14 23:00:35 +0300

    Update example_parallel_function_launch_connector.php

--- cron/inc/example_parallel_function_launch_connector.php
+++ cron/inc/example_parallel_function_launch_connector.php
@@ -1,6 +1,6 @@
 <?php
 // Example send param, before include('cron.php'):
-// works in the same subdirectory with the program
+// works in the same subdirectory with the cron.php
 
 ###########################
 	// frame - pushed frame (string)
