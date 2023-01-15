7c93a6b2d43ca004bd618bc8ee9ec8dc7632d339
Евгений <40506054+commeta@users.noreply.github.com>
2023-01-15 21:48:49 +0300

    Update callback_cron.php

--- cron/inc/callback_cron.php
+++ cron/inc/callback_cron.php
@@ -1,11 +1,11 @@
 <?php
 
 // Write log
-if($cron_settings['log_level'] > 2){
-	if($cron_settings['log_file']){
-		@file_put_contents(
-			$cron_settings['log_file'], 
-			sprintf("%d INFO: start %d %d %d \n", time(), getmypid(), $job_process_id, $cron_session['start_counter']),
+if($cron_requests_events_settings['log_level'] > 2){
+	if($cron_requests_events_settings['log_file']){
+		file_put_contents(
+			$cron_requests_events_settings['log_file'], 
+			sprintf("%d INFO: start %d %d %d \n", time(), getmypid(), $job_process_id, $cron_requests_events_session['start_counter']),
 			FILE_APPEND | LOCK_EX
 		);
 	}
@@ -14,10 +14,10 @@ if($cron_settings['log_level'] > 2){
 
 
 // Save sesson variables, in Job context, autoload in next start session
-if(isset($cron_session['start_counter'])){
-	$cron_session['start_counter']++;
+if(isset($cron_requests_events_session['start_counter'])){
+	$cron_requests_events_session['start_counter']++;
 } else {
-	$cron_session['start_counter']= 0;
+	$cron_requests_events_session['start_counter']= 0;
 }
 
 
