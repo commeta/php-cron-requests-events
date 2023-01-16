39ed2f84f7fafb8ecbec95a9f63d37e239e8e588
Евгений <40506054+commeta@users.noreply.github.com>
2023-01-16 23:00:29 +0300

    Update cron.php

--- cron.php
+++ cron.php
@@ -29,6 +29,8 @@
 // declare(strict_types = 1); // strict typing PHP > 7.0
 
 ###########################
+###########################
+###########################
 # System dirs
 
 $cron_requests_events_root= dirname(__FILE__) . DIRECTORY_SEPARATOR;
@@ -59,7 +61,7 @@ date_default_timezone_set('Europe/Moscow');
 
 
 ###########################
-# EXAMPLES
+# EXAMPLES 
 $cron_requests_events_jobs= [];
 
 ###########################
@@ -117,8 +119,13 @@ $cron_requests_events_jobs[]= [ // CRON Job 4, multithreading example
 
 
 ###########################
+###########################
+###########################
 # Settings parallel function start
-if(isset($_GET['parallel_start']) && !isset($cron_requests_events_start)){
+if(
+	isset($_GET['parallel_start']) && 
+	!isset($cron_requests_events_start)
+){ // Init multi-threading launch
 	$process_id= getmypid();
 	
 	$cron_requests_events_settings['log_file']= '';
@@ -158,13 +165,24 @@ if($cron_requests_events_settings['queue_file'] !== ''){
 
 
 ###########################
-# Public functions
+# example: parallel launch functions
+if(!function_exists('send_param_and_parallel_launch')) { 
+	function send_param_and_parallel_launch($params, $frame_size){ 
+		global $cron_requests_events_settings;
+		
+		queue_address_push($params, $frame_size);
+		
+		if(function_exists('open_cron_socket')) {
+			open_cron_socket($cron_requests_events_settings['url_key'], '', true);
+		}
+	}
+}
 
 if(isset($_REQUEST["cron"])):
 
 	function get_param($process_id){ // Example get param, function called in parallel process cron.php
 		global $cron_requests_events_settings, $cron_requests_events_resource, $cron_requests_events_log;
-		$frame_size= 64;
+		$frame_size= 96;
 
 		while(true){ // example: loop from the end
 			$frame= queue_address_pop($frame_size);
@@ -196,231 +214,18 @@ if(isset($_REQUEST["cron"])):
 		
 		_die();
 	}
-	
 endif;
 
 
-if(!function_exists('send_param_and_parallel_launch')) { 
-	function send_param_and_parallel_launch($params, $frame_size){ 
-		global $cron_requests_events_settings;
-		
-		queue_address_push($params, $frame_size);
-		
-		if(function_exists('open_cron_socket')) {
-			open_cron_socket($cron_requests_events_settings['url_key'], '', true);
-		}
-	}
-}
-
-
-if(!function_exists('open_cron_socket')) { 
-	function open_cron_socket($cron_requests_events_url_key, $job_process_id= '', $parallel_start= false) // :void 
-	{ // Start job in parallel process
-		if(
-			isset($_SERVER['HTTPS']) &&
-			($_SERVER['HTTPS'] === 'on' || $_SERVER['HTTPS'] === 1) ||
-			isset($_SERVER['HTTP_X_FORWARDED_PROTO']) &&
-			$_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https'
-		) {
-			$protocol= 'https';
-		} else {
-			$protocol= 'http';
-		}
-
-		$document_root= preg_match('/\/$/',$_SERVER["DOCUMENT_ROOT"]) ? $_SERVER["DOCUMENT_ROOT"] : $_SERVER["DOCUMENT_ROOT"].DIRECTORY_SEPARATOR;
-		
-		if(isset($_SERVER["HTTP_HOST"])) {
-			$host= strtolower($_SERVER["HTTP_HOST"]);
-						
-			if($job_process_id !== '') $cron_requests_events_url_key.= '&job_process_id=' . $job_process_id;
-		} elseif( // sapi: CLI
-			defined('STDIN') ||
-			php_sapi_name() === 'cli' ||
-			array_key_exists('SHELL', $_ENV) ||
-			!array_key_exists('REQUEST_METHOD', $_SERVER)
-		) {
-			$protocol= 'https';
-			$host= "localhost";
-			$document_root= dirname(__FILE__) . DIRECTORY_SEPARATOR; // site root path
-			
-			echo "Request: " . $protocol . '://' . $host . "/" . basename(__FILE__) . "?cron=" . $cron_requests_events_url_key . "\n";
-			echo 'or change $host= "localhost" to your domain' . "\n";
-		}
-		
-		$cron_requests_events_url= $protocol . '://' . $host . '/' . 
-			str_replace($document_root , '', dirname(__FILE__) . DIRECTORY_SEPARATOR) . 
-			basename(__FILE__) ."?cron=" . $cron_requests_events_url_key;
-			
-			
-		if($parallel_start !== false) $cron_requests_events_url.= '&parallel_start=true';
-			
-		if(
-			is_callable("shell_exec") && 
-			defined("PHP_BINDIR") && 
-			is_executable(PHP_BINDIR . DIRECTORY_SEPARATOR . 'php') 
-		){
-			if($protocol ===  'https') {
-				$stream_context= '["ssl"=>["verify_peer"=>false,"verify_peer_name"=>false],"http"=>["timeout"=>1]]';
-			} else {
-				$stream_context= '["http"=>["timeout"=>1]]';
-			}
-			
-			shell_exec(
-					PHP_BINDIR . DIRECTORY_SEPARATOR . 'php -r \'file_get_contents("' . 
-					$cron_requests_events_url . '", false, stream_context_create('. 
-					$stream_context . '));\' > /dev/null 2>/dev/null &'
-			);
-			
-		} else {
-			@fclose( 
-				@fopen(
-					$cron_requests_events_url, 
-					'r', 
-					false, 
-					stream_context_create([ // block mode
-						'http'=>[ // script responce time 0.004 * 10 (Linux Kernel Load Average\OPcache\FLOPS) = 0.04 timeout start process
-							'timeout' => 0.04 // it will be necessary to increase for high loaded systems
-						]
-					])
-				)
-			);
-		}
-	}
-}
-
-
-if(!function_exists('queue_address_push')) { 
-	// frame - pushed frame (string)
-	// frame_size - set frame size (int)
-	// frame_cursor - PHP_INT_MAX for LIFO mode, get frame from cursor position (int)
-	// return frame cursor offset (int), 0 if error or boot frame
-	function queue_address_push($frame, $frame_size= 0, $frame_cursor= PHP_INT_MAX, $callback= '') // :int 
-	{ // push data frame in stack
-		global $cron_requests_events_settings;
-		
-		$queue_resource= fopen($cron_requests_events_settings['queue_file'], "r+");
-		$return_cursor= 0;
-
-		if($frame_size !== 0){
-			if($frame_size < mb_strlen($frame)){ // fill
-				return $return_cursor;
-			}
-		} else {
-			return $return_cursor;
-		}
-
-		if(flock($queue_resource, LOCK_EX)) {
-			if($callback !== '') @call_user_func($callback, $queue_resource, $frame_size, $frame_cursor); // callback anonymous
-			
-			$stat= fstat($queue_resource);
-			$frame_length= mb_strlen($frame);
-			
-			if($frame_length < $frame_size){
-				for($i= $frame_length; $i <= $frame_size; $i++) $frame.= chr(0);
-			}
-
-			if($frame_cursor !== PHP_INT_MAX){
-				$return_cursor= $frame_cursor;				
-				fseek($queue_resource, $frame_cursor);
-				fwrite($queue_resource, $frame, $frame_size);
-				fflush($queue_resource);
-			} else {
-				$return_cursor= $stat['size'];
-				fseek($queue_resource, $stat['size']);
-				fwrite($queue_resource, $frame, $frame_size);
-				fflush($queue_resource);
-			}
-			
-			flock($queue_resource, LOCK_UN);
-		}
-		
-		fclose($queue_resource);
-		return $return_cursor;
-	}
-}
-
-
-if(!function_exists('queue_address_pop')) { 
-	// frame_size - set frame size (int)
-	// frame_cursor - PHP_INT_MAX for LIFO mode, get frame from cursor position (int)
-	// frame_replace - empty('') is off, replace frame (string)
-	// return value from stack frame, empty string '' if error or lifo queue end (string)
-	function queue_address_pop($frame_size= 0, $frame_cursor= PHP_INT_MAX, $frame_replace= '', $callback= '') // :string 
-	{ // pop data frame from stack
-		global $cron_requests_events_settings;
-		
-		$queue_resource= fopen($cron_requests_events_settings['queue_file'], "r+");
-		$frame= '';
-		
-		if(flock($queue_resource, LOCK_EX)) {
-			if($callback !== '') @call_user_func($callback, $queue_resource, $frame_size, $frame_cursor, $frame_replace); // callback anonymous
-			
-			$stat= fstat($queue_resource);
-
-			if($stat['size'] < 1){ // queue file is empty
-				flock($queue_resource, LOCK_UN);
-				fclose($queue_resource);
-				return $frame;
-			}
-
-			if($frame_cursor !== PHP_INT_MAX){
-				$cursor= $frame_cursor;
-			} else {
-				if($stat['size'] - $frame_size > 0) $cursor= $stat['size'] - $frame_size;
-				else $cursor= 0;
-			}
-
-			fseek($queue_resource, $cursor); // get data frame
-			$v= trim(fread($queue_resource, $frame_size));
-			if(mb_strlen($v) > 0) $frame= $v;
-			
-			if($frame_cursor !== PHP_INT_MAX){
-				if($frame_replace !== ''){ // replace frame
-					$length_frame_replace= mb_strlen($frame_replace);
-					
-					if($length_frame_replace <= $frame_size){
-						if($length_frame_replace < $frame_size){
-							for($i= $length_frame_replace; $i <= $frame_size; $i++) $frame_replace.= chr(0);
-						}
-						
-						fseek($queue_resource, $cursor); 
-						fwrite($queue_resource, $frame_replace, $frame_size);
-						fflush($queue_resource);
-					} else {
-						return '';
-					}
-				}
-				
-			} elseif(mb_strlen($v) > 0) { // LIFO mode	
-				if($stat['size'] - $frame_size >= 0) $trunc= $stat['size'] - $frame_size;
-				else $trunc= 0; // truncate file
-
-				ftruncate($queue_resource, $trunc);
-				fflush($queue_resource);
-			}
-			
-			flock($queue_resource, LOCK_UN);
-		}
-		
-		fclose($queue_resource);
-		return $frame;
-	}
-}
-
-
-
 ###########################
-# Main functions, system api 
+# example: multicore queue
 if(
 	isset($_REQUEST["cron"]) &&
 	$_REQUEST["cron"] === $cron_requests_events_settings['url_key']
-){
-
+):
 	function queue_address_manager($mode) // :void 
-	{ // example: multicore queue
+	{
 		global $cron_requests_events_settings;
-		
-		// Init
 		$frame_size= 95;
 		$process_id= getmypid();
 		$frame_completed= serialize([true]);
@@ -623,8 +428,218 @@ if(
 			
 		}
 	}
-	
-	
+endif;
+
+
+
+###########################
+###########################
+###########################
+# Public functions, userspace
+if(!function_exists('open_cron_socket')) { 
+	function open_cron_socket($cron_requests_events_url_key, $job_process_id= '', $parallel_start= false) // :void 
+	{ // Start job in parallel process
+		if(
+			isset($_SERVER['HTTPS']) &&
+			($_SERVER['HTTPS'] === 'on' || $_SERVER['HTTPS'] === 1) ||
+			isset($_SERVER['HTTP_X_FORWARDED_PROTO']) &&
+			$_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https'
+		) {
+			$protocol= 'https';
+		} else {
+			$protocol= 'http';
+		}
+
+		$document_root= preg_match('/\/$/',$_SERVER["DOCUMENT_ROOT"]) ? $_SERVER["DOCUMENT_ROOT"] : $_SERVER["DOCUMENT_ROOT"].DIRECTORY_SEPARATOR;
+		
+		if(isset($_SERVER["HTTP_HOST"])) {
+			$host= strtolower($_SERVER["HTTP_HOST"]);
+						
+			if($job_process_id !== '') $cron_requests_events_url_key.= '&job_process_id=' . $job_process_id;
+		} elseif( // sapi: CLI
+			defined('STDIN') ||
+			php_sapi_name() === 'cli' ||
+			array_key_exists('SHELL', $_ENV) ||
+			!array_key_exists('REQUEST_METHOD', $_SERVER)
+		) {
+			$protocol= 'https';
+			$host= "localhost";
+			$document_root= dirname(__FILE__) . DIRECTORY_SEPARATOR; // site root path
+			
+			echo "Request: " . $protocol . '://' . $host . "/" . basename(__FILE__) . "?cron=" . $cron_requests_events_url_key . "\n";
+			echo 'or change $host= "localhost" to your domain' . "\n";
+		}
+		
+		$cron_requests_events_url= $protocol . '://' . $host . '/' . 
+			str_replace($document_root , '', dirname(__FILE__) . DIRECTORY_SEPARATOR) . 
+			basename(__FILE__) ."?cron=" . $cron_requests_events_url_key;
+			
+			
+		if($parallel_start !== false) $cron_requests_events_url.= '&parallel_start=true';
+			
+		if(
+			is_callable("shell_exec") && 
+			defined("PHP_BINDIR") && 
+			is_executable(PHP_BINDIR . DIRECTORY_SEPARATOR . 'php') 
+		){
+			if($protocol ===  'https') {
+				$stream_context= '["ssl"=>["verify_peer"=>false,"verify_peer_name"=>false],"http"=>["timeout"=>1]]';
+			} else {
+				$stream_context= '["http"=>["timeout"=>1]]';
+			}
+			
+			shell_exec(
+				PHP_BINDIR . DIRECTORY_SEPARATOR . 'php -r \'file_get_contents("' . 
+				$cron_requests_events_url . '", false, stream_context_create('. 
+				$stream_context . '));\' > /dev/null 2>/dev/null &'
+			);
+			
+		} else {
+			@fclose( 
+				@fopen(
+					$cron_requests_events_url, 
+					'r', 
+					false, 
+					stream_context_create([ // block mode
+						'http'=>[ // script responce time 0.004 * 10 (Load Average\OPcache\FLOPS) = 0.04 timeout start process
+							'timeout' => 0.04 // it will be necessary to increase for high loaded systems
+						]
+					])
+				)
+			);
+		}
+	}
+}
+
+
+if(!function_exists('queue_address_push')) { 
+	// frame - pushed frame (string)
+	// frame_size - set frame size (int)
+	// frame_cursor - PHP_INT_MAX for LIFO mode, get frame from cursor position (int)
+	// return frame cursor offset (int), 0 if error or boot frame
+	function queue_address_push($frame, $frame_size= 0, $frame_cursor= PHP_INT_MAX, $callback= '') // :int 
+	{ // push data frame in stack
+		global $cron_requests_events_settings;
+		
+		$queue_resource= fopen($cron_requests_events_settings['queue_file'], "r+");
+		$return_cursor= 0;
+
+		if($frame_size !== 0){
+			if($frame_size < mb_strlen($frame)){ // fill
+				return $return_cursor;
+			}
+		} else {
+			return $return_cursor;
+		}
+
+		if(flock($queue_resource, LOCK_EX)) {
+			if($callback !== '') @call_user_func($callback, $queue_resource, $frame_size, $frame_cursor); // callback anonymous
+			
+			$stat= fstat($queue_resource);
+			$frame_length= mb_strlen($frame);
+			
+			if($frame_length < $frame_size){
+				for($i= $frame_length; $i <= $frame_size; $i++) $frame.= chr(0);
+			}
+
+			if($frame_cursor !== PHP_INT_MAX){
+				$return_cursor= $frame_cursor;				
+				fseek($queue_resource, $frame_cursor);
+				fwrite($queue_resource, $frame, $frame_size);
+				fflush($queue_resource);
+			} else {
+				$return_cursor= $stat['size'];
+				fseek($queue_resource, $stat['size']);
+				fwrite($queue_resource, $frame, $frame_size);
+				fflush($queue_resource);
+			}
+			
+			flock($queue_resource, LOCK_UN);
+		}
+		
+		fclose($queue_resource);
+		return $return_cursor;
+	}
+}
+
+
+if(!function_exists('queue_address_pop')) { 
+	// frame_size - set frame size (int)
+	// frame_cursor - PHP_INT_MAX for LIFO mode, get frame from cursor position (int)
+	// frame_replace - empty('') is off, replace frame (string)
+	// return value from stack frame, empty string '' if error or lifo queue end (string)
+	function queue_address_pop($frame_size= 0, $frame_cursor= PHP_INT_MAX, $frame_replace= '', $callback= '') // :string 
+	{ // pop data frame from stack
+		global $cron_requests_events_settings;
+		
+		$queue_resource= fopen($cron_requests_events_settings['queue_file'], "r+");
+		$frame= '';
+		
+		if(flock($queue_resource, LOCK_EX)) {
+			if($callback !== '') @call_user_func($callback, $queue_resource, $frame_size, $frame_cursor, $frame_replace); // callback anonymous
+			
+			$stat= fstat($queue_resource);
+
+			if($stat['size'] < 1){ // queue file is empty
+				flock($queue_resource, LOCK_UN);
+				fclose($queue_resource);
+				return $frame;
+			}
+
+			if($frame_cursor !== PHP_INT_MAX){
+				$cursor= $frame_cursor;
+			} else {
+				if($stat['size'] - $frame_size > 0) $cursor= $stat['size'] - $frame_size;
+				else $cursor= 0;
+			}
+
+			fseek($queue_resource, $cursor); // get data frame
+			$v= trim(fread($queue_resource, $frame_size));
+			if(mb_strlen($v) > 0) $frame= $v;
+			
+			if($frame_cursor !== PHP_INT_MAX){
+				if($frame_replace !== ''){ // replace frame
+					$length_frame_replace= mb_strlen($frame_replace);
+					
+					if($length_frame_replace <= $frame_size){
+						if($length_frame_replace < $frame_size){
+							for($i= $length_frame_replace; $i <= $frame_size; $i++) $frame_replace.= chr(0);
+						}
+						
+						fseek($queue_resource, $cursor); 
+						fwrite($queue_resource, $frame_replace, $frame_size);
+						fflush($queue_resource);
+					} else {
+						return '';
+					}
+				}
+				
+			} elseif(mb_strlen($v) > 0) { // LIFO mode	
+				if($stat['size'] - $frame_size >= 0) $trunc= $stat['size'] - $frame_size;
+				else $trunc= 0; // truncate file
+
+				ftruncate($queue_resource, $trunc);
+				fflush($queue_resource);
+			}
+			
+			flock($queue_resource, LOCK_UN);
+		}
+		
+		fclose($queue_resource);
+		return $frame;
+	}
+}
+
+
+
+###########################
+###########################
+###########################
+# Main functions, system api 
+if(
+	isset($_REQUEST["cron"]) &&
+	$_REQUEST["cron"] === $cron_requests_events_settings['url_key']
+){
 	function write_cron_session() // :void 
 	{
 		global  $cron_requests_events_resource, $cron_requests_events_session;
