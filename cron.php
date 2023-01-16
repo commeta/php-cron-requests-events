c94dfb8f17f478aed3c90b07a73d617d4cd25682
Евгений <40506054+commeta@users.noreply.github.com>
2023-01-17 00:58:11 +0300

    Update cron.php

--- cron.php
+++ cron.php
@@ -226,7 +226,7 @@ if(
 	function queue_address_manager($mode) // :void 
 	{
 		global $cron_requests_events_settings;
-		$frame_size= 95;
+		$frame_size= 96;
 		$process_id= getmypid();
 		$frame_completed= serialize([true]);
 
@@ -824,6 +824,7 @@ if(
 		}
 	}
 			
+	
 	function cron_session_init($job, $job_process_id) // :void 
 	{
 		global $cron_requests_events_session;
@@ -850,6 +851,98 @@ if(
 			$cron_requests_events_session[$job_process_id]['complete']= false;
 		}
 	}
+	
+	
+	
+	###########################
+	# Provides basic cron syntax parsing functionality
+	#
+	# @author:  Jan Konieczny <jkonieczny@gmail.com>
+	# @copyright: Copyright (C) 2009, Jan Konieczny
+	#
+	#    
+	# Finds next execution time(stamp) parsin crontab syntax,
+	# after given starting timestamp (or current time if ommited)
+	#  
+	# @param string $_cron_string :
+	#  
+	# 0    1    2    3    4
+	# *    *    *    *    *
+	# -    -    -    -    -
+	# |    |    |    |    |
+	# |    |    |    |    +----- day of week (0 - 6) (Sunday=0)
+	# |    |    |    +------- month (1 - 12)
+	# |    |    +--------- day of month (1 - 31)
+	# |    +----------- hour (0 - 23)
+	# +------------- min (0 - 59)
+	# @param int $_after_timestamp timestamp [default=current timestamp]
+	# @return int unix timestamp - next execution time will be greater
+	#               than given timestamp (defaults to the current timestamp)
+	# @throws \InvalidArgumentException
+	#
+	function parse($_cron_string, $_after_timestamp = null) // : int
+	{
+		if(
+			!preg_match('/^((\*(\/[0-9]+)?)|[0-9\-\,\/]+)\s+((\*(\/[0-9]+)?)|[0-9\-\,\/]+)\s+((\*(\/[0-9]+)?)|[0-9\-\,\/]+)\s+((\*(\/[0-9]+)?)|[0-9\-\,\/]+)\s+((\*(\/[0-9]+)?)|[0-9\-\,\/]+)$/i', trim($_cron_string))
+		){
+			return 0; // Invalid cron string:  $_cron_string
+		}
+        
+		if($_after_timestamp && !is_numeric($_after_timestamp)){
+			return 0; // InvalidArgumentException $_after_timestamp must be a valid unix timestamp ($_after_timestamp given)
+		}
+        
+		$cron = preg_split("/[\s]+/i", trim($_cron_string));
+		$start = empty($_after_timestamp) ? time() : $_after_timestamp;
+		$date = array('minutes' => parseCronNumbers($cron[0], 0, 59),
+			'hours' => parseCronNumbers($cron[1], 0, 23),
+			'dom' => parseCronNumbers($cron[2], 1, 31),
+			'month' => parseCronNumbers($cron[3], 1, 12),
+			'dow' => parseCronNumbers($cron[4], 0, 6),
+		);
+        
+		// limited to time()+366 - no need to check more than 1year ahead
+		for ($i = 0; $i <= 60 * 60 * 24 * 366; $i += 60) {
+			if (in_array(intval(date('j', $start + $i)), $date['dom']) &&
+				in_array(intval(date('n', $start + $i)), $date['month']) &&
+				in_array(intval(date('w', $start + $i)), $date['dow']) &&
+				in_array(intval(date('G', $start + $i)), $date['hours']) &&
+				in_array(intval(date('i', $start + $i)), $date['minutes'])
+			) {
+				return $start + $i;
+			}
+		}
+		return 0;
+	}
+
+	###########################
+	# Provides basic cron syntax parsing functionality
+	# get a single cron style notation and parse it into numeric value
+
+	#  @param string $s cron string element
+	#  @param int $min minimum possible value
+	#  @param int $max maximum possible value
+	#  @return int parsed number
+
+	function parseCronNumbers($s, $min, $max) // : array
+	{
+		$result = array();
+		$v = explode(',', $s);
+		foreach ($v as $vv) {
+			$vvv = explode('/', $vv);
+			$step = empty($vvv[1]) ? 1 : $vvv[1];
+			$vvvv = explode('-', $vvv[0]);
+			$_min = count($vvvv) == 2 ? $vvvv[0] : ($vvv[0] == '*' ? $min : $vvv[0]);
+			$_max = count($vvvv) == 2 ? $vvvv[1] : ($vvv[0] == '*' ? $max : $vvv[0]);
+			for ($i = $_min; $i <= $_max; $i += $step) {
+				$result[$i] = intval($i);
+			}
+		}
+		ksort($result);
+
+		return $result;
+	}	
+	
 	
 	function cron_check_job($job, $job_process_id, $mode) // :void 
 	{
@@ -897,6 +990,22 @@ if(
 				$cron_requests_events_session[$job_process_id]['complete']= true;
 				$cron_requests_events_session[$job_process_id]['last_update']= time();
 			}
+			
+		} elseif(isset($job['crontab'])){ // crontab syntax, bug fix!
+			if($cron_requests_events_session[$job_process_id]['last_update'] === 0){
+				$crontab= parse($job['crontab']);
+				if($crontab !== 0) $cron_requests_events_session[$job_process_id]['last_update']= $crontab;
+			}
+			
+			if(
+				$cron_requests_events_session[$job_process_id]['last_update'] < $time
+			){
+				$crontab= parse($job['crontab']);
+				callback_connector($job, $job_process_id, $mode);
+				$cron_requests_events_session[$job_process_id]['complete']= true;
+				if($crontab !== 0) $cron_requests_events_session[$job_process_id]['last_update']= $crontab;
+			}
+			
 		} else {
 			if(
 				$cron_requests_events_session[$job_process_id]['last_update'] + $job['interval'] < $time
