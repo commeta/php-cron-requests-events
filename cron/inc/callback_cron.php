19111802f31cb0c0efd3e8b5bfae8f39ec8a99d7
Евгений <40506054+commeta@users.noreply.github.com>
2023-01-17 04:19:12 +0300

    Update callback_cron.php

--- cron/inc/callback_cron.php
+++ cron/inc/callback_cron.php
@@ -1,9 +1,8 @@
 <?php
 
 // Write log
-if($cron_requests_events_settings['log_level'] > 2){
-	cron_log(sprintf("%d INFO: start %d %d %d", time(), getmypid(), $job_process_id, $cron_requests_events_session['start_counter']));
-}
+cron_log(sprintf("%d INFO: start %d %d %d", time(), getmypid(), $job_process_id, $cron_requests_events_session['start_counter']), 5);
+
 
 
 
